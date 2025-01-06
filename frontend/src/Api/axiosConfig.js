83dafe72c075b56f6ef1cd8e59f620fa45d6667b
Your Name <you@example.com>
2025-01-06 23:07:44 +0100

    api axios

--- frontend/src/Api/axiosConfig.js
+++ frontend/src/Api/axiosConfig.js
@@ -15,7 +15,7 @@ const axiosBase = axios.create({
   // baseURL: "http://localhost:5000/api",
 
   // For production on Render:
-  baseURL: "https://evangadi-forum-fs-grp3a.onrender.com/api",
+  baseURL: "https://evangadi-forum-fs-grp3a.onrender.com/api/users",
 });
 
 export default axiosBase;
